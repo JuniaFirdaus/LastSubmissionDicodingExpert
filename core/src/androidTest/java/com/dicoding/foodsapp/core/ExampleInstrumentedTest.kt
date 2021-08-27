fa0dbc88f589869544aec4579a6c6da290c52fa9
Junia Firdaus <firdausjunia@gmail.com>
2021-08-27 18:33:05 +0700

    add proguard-rules

--- core/src/androidTest/java/com/dicoding/foodsapp/core/ExampleInstrumentedTest.kt
+++ core/src/androidTest/java/com/dicoding/foodsapp/core/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.dicoding.tourismapp.core.test", appContext.packageName)
+        assertEquals("com.dicoding.foodsapp.core.test", appContext.packageName)
     }
 }
